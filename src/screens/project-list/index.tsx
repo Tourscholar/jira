db8a980857568b4b4e3cc6689ba950c4deafb71a
Tourscholar <krj1608178365@gmail.com>
2022-02-24 21:07:00 +0800

    feat: add login status maintenance

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,9 +2,7 @@ import { SearchPanel } from "./searchPanel";
 import { List } from "./list";
 import { useEffect, useState } from "react";
 import { cleanObject, useDebounce, useMount } from "../../utils";
-import * as qs from "qs";
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import { useHttp } from "utils/http";
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
@@ -18,22 +16,14 @@ export const ProjectListScreen = () => {
 
   const [list, setList] = useState([]);
 
+  const client = useHttp();
+
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
